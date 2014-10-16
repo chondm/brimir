8fc80544a03cf22e2733033add627e2a1137f74d
Chon Doan <domich.dt@gmail.com>
2014-10-17 01:09:01 +0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,139 @@
-set :application, 'brimir'
-set :repo_url, 'git@github.com:chondm/brimir.git'
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+require 'mina/unicorn'
+# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
-set :deploy_to, '/home/deployer'
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
 
-#set :linked_files, %w{config/database.yml}
-#set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :domain, '104.131.3.63'
+set :deploy_to, '/home/deployer/brimir'
+set :repository, 'git@github.com:chondm/brimir.git'
+set :branch, 'master'
+set :user, 'deployer'
+set :forward_agent, true
+set :port, '22'
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
-namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-      #execute "mkdir #{deploy_to}/current/tmp; touch #{deploy_to}/tmp/restart.txt"
+
+
+set :shared_paths, ['config/database.yml', 'log', 'config/secrets.yml']
+
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  queue %{
+echo "-----> Loading environment"
+#{echo_cmd %[source ~/.bashrc]}
+}
+  invoke :'rbenv:load'
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .rbenv-version to your repository.
+end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+
+  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
+  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+
+  queue! %[touch "#{deploy_to}/shared/config/secrets.yml"]
+  queue %[echo "-----> Be sure to edit 'shared/config/secrets.yml'."]
+
+  # sidekiq needs a place to store its pid file and log file
+  queue! %[mkdir -p "#{deploy_to}/shared/pids/"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/pids"]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+
+    # stop accepting new workers
+
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+
+    to :launch do
+      invoke :'unicorn:restart'
     end
   end
-
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
 end
+
+
+
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+#set :shared_paths, ['config/database.yml', 'log']
+
+# Optional settings:
+#   set :user, 'foobar'    # Username in the server to SSH to.
+#   set :port, '30000'     # SSH port number.
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+#task :environment do
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .rbenv-version to your repository.
+  # invoke :'rbenv:load'
+
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+#end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+#task :setup => :environment do
+#  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+#  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+#
+#  queue! %[mkdir -p "#{deploy_to}/shared/config"]
+#  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+#
+#  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
+#  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+#end
+#
+#desc "Deploys the current version to the server."
+#task :deploy => :environment do
+#  deploy do
+#    # Put things that will set up an empty directory into a fully set-up
+#    # instance of your project.
+#    invoke :'git:clone'
+#    invoke :'deploy:link_shared_paths'
+#    invoke :'bundle:install'
+#    invoke :'rails:db_migrate'
+#    invoke :'rails:assets_precompile'
+#
+#    to :launch do
+#      queue "touch #{deploy_to}/tmp/restart.txt"
+#    end
+#  end
+#end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - http://nadarei.co/mina
+#  - http://nadarei.co/mina/tasks
+#  - http://nadarei.co/mina/settings
+#  - http://nadarei.co/mina/helpers
+
