1ed3f68cddc23221b1e2bd917b28a4a4f8cd2fa5
Chon Doan <domich.dt@gmail.com>
2014-10-17 17:08:07 +0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140829124853) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "attachments", force: true do |t|
     t.integer  "attachable_id"
     t.string   "attachable_type"
